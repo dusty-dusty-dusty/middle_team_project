b863fc48263abbb5ee04d8da1ff46cf15aa5e12a
SHL <shl.ictedu@gmail.com>
2025-06-02 00:59:24 +0900

    250601_LHS

--- deskset/src/main/java/com/mid/manager/model/RegistProductDTO.java
+++ deskset/src/main/java/com/mid/manager/model/RegistProductDTO.java
@@ -1,5 +1,8 @@
 package com.mid.manager.model;
 
+import lombok.Data;
+
+@Data
 public class RegistProductDTO {
 	private String cate_id;
 	private String admin_no;
@@ -8,15 +11,9 @@ public class RegistProductDTO {
 	private String product_mfc;
 	private Integer product_price;
 	private Integer product_stock;
-	
-	private String attr_id;
-	private String attr_value;
-	
+
 	private String product_image;
 	private String product_thum;
 	private String product_cont;
-	
-	
-	
 	
 }
--- deskset/src/main/java/com/mid/manager/service/AdminProductService.java
+++ deskset/src/main/java/com/mid/manager/service/AdminProductService.java
@@ -4,8 +4,10 @@ import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 
+import com.mid.manager.model.AttrValueVO;
 import com.mid.manager.model.CategoryVO;
 import com.mid.manager.model.ProductCAJoinVO;
+import com.mid.manager.model.RegistProductDTO;
 import com.mid.manager.model.RegistProductVO;
 
 public interface AdminProductService {
@@ -18,5 +20,16 @@ public interface AdminProductService {
 
 	ResponseEntity<List<RegistProductVO>> getcategoryATTR(String category);
 	
+	
+	//카테고리명으로 카테고리 ID 조회
+    String getCategoryIdByName(String categoryName);
+    
+    
+    //상품 등록
+    //@return 등록된 상품 번호
+    String registerProduct(RegistProductDTO productDTO);
+    
+    //속성값 저장
+    void insertAttrValue(AttrValueVO attrValueVO);
 
 }
