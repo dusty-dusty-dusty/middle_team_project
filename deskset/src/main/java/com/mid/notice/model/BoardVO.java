648e10d7f78925644ec678fcef02f8ad3ea921cb
sangHyeon7217 <93038215+sangHyeon7217@users.noreply.github.com>
2025-06-02 11:54:59 +0900

    ignore추가_HSH

--- deskset/src/main/java/com/mid/notice/model/BoardVO.java
+++ deskset/src/main/java/com/mid/notice/model/BoardVO.java
@@ -6,7 +6,7 @@ import lombok.Data;
 
 @Data
 public class BoardVO {
-	 private String notice_no;
+	private String notice_no;
 	private String notice_title;
 	private String notice_cont;
 	private Date notice_date;
